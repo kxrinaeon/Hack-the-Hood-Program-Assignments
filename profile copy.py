6f6f892eb5ecc7376dc81bb7698dc6ebe5255809
Karina Ferreira <karinaferreira@Karinas-MacBook-Pro.local>
2026-06-22 12:52:57 -0700

    clone repo

---
+++ profile copy.py
@@ -0,0 +1,16 @@
+full_name = "Karina Ferreira Gandara"
+nickname = "Rina"
+age = 18
+used_python_before = True
+hobbies = ["gaming", "coding", "drawing"]
+favorites = {
+    "movie": "The Book of Life",
+    "food": "Carne Asada",
+    "hobby": "gaming"
+}
+
+print("Name:", full_name)
+print("Age:", age)
+print("Hobbies:", hobbies)
+print("Favorite Stuff:", favorites)
+print("Has used python before:", used_python_before)
---
+++ variable.py
@@ -0,0 +1,11 @@
+first_name = "Karina"
+last_name = "Ferreira Gandara"
+age = 18
+
+name = first_name + " " + last_name
+
+print(first_name)
+print(last_name)
+print(name)
+print(first_name, last_name)
+print(first_name, last_name, "is", str(age), "years old.")
